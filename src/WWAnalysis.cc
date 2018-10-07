0b99ff34c97160449bf33fd0e7b55d7c03beb615
Jphsx <justin.anguiano1@gmail.com>
2018-10-06 19:04:17 -0500

    fixing jet multiplicities

--- src/WWAnalysis.cc
+++ src/WWAnalysis.cc
@@ -198,7 +198,7 @@ void WWAnalysis::exploreDaughterTracks(MCParticle* p){
 	std::vector<MCParticle*> d = p->getDaughters();
 	for(int i=0; i< d.size(); i++){
 		if( ! d.at(i)->isCreatedInSimulation() ){
-			std::cout<< d->getPDG() <<" ";
+			std::cout<< d.at(i)->getPDG() <<" ";
 		}
 	}
 	std::cout<<std::endl;
